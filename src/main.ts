b70a6bcc3cd5a038a82ac2747e423c41aea6a27e
Francis Lagares <francis.lagares@gmail.com>
2021-08-29 14:13:36 +0200

    feat: setup typeorm and jwt env variables with ConfigModule

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,18 @@
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { TransformInterceptor } from './transform.interceptor';
 
 async function bootstrap() {
+  const logger = new Logger();
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
 
+  const port = 3000;
   await app.listen(3000);
+  logger.log(`Application running on port ${port}`);
 }
 bootstrap();
